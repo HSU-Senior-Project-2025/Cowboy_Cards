1607ace796028feb10e1309807a3d7ecebdfc2f1
Stephen Lajuwomi <111301988+SLajuwomi@users.noreply.github.com>
2025-02-28 14:29:41 -0600

    changed formatting to resolve conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
-import { IonReactRouter } from "@ionic/react-router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Route } from "react-router-dom";
-import ClassDetail from "./pages/ClassDetail";
-import Home from "./pages/Home";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-import PublicCards from "./pages/PublicCards";
-import TeacherDashboard from "./pages/TeacherDashboard";
+import { Toaster as Sonner } from '@/components/ui/sonner';
+import { Toaster } from '@/components/ui/toaster';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Route } from 'react-router-dom';
+import ClassDetail from './pages/ClassDetail';
+import Home from './pages/Home';
+import Index from './pages/Index';
+import NotFound from './pages/NotFound';
+import PublicCards from './pages/PublicCards';
+import TeacherDashboard from './pages/TeacherDashboard';
 
 /* Core CSS required for Ionic components to work properly */
-import "@ionic/react/css/core.css";
+import '@ionic/react/css/core.css';
 
 /* Basic CSS for apps built with Ionic */
-import "@ionic/react/css/normalize.css";
-import "@ionic/react/css/structure.css";
-import "@ionic/react/css/typography.css";
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
 
 /* Optional CSS utils that can be commented out */
-import "@ionic/react/css/display.css";
-import "@ionic/react/css/flex-utils.css";
-import "@ionic/react/css/float-elements.css";
-import "@ionic/react/css/padding.css";
-import "@ionic/react/css/text-alignment.css";
-import "@ionic/react/css/text-transformation.css";
+import '@ionic/react/css/display.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
 
 /* Theme variables */
-import "./theme/variables.css";
+import './theme/variables.css';
 
 setupIonicReact();
 
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -3,7 +3,6 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const NotFound = () => {
-
   const location = useLocation();
 
   useEffect(() => {
