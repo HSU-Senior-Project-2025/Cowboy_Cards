3bf18eb8e7b426161090f03babeb02abd5474bb8
evanhanson <eph2201@hsutx.edu>
2025-03-05 15:21:19 -0600

    fixing up delete flashcard_sets endpoint and sqlc code

--- go/routes/routes.go
+++ go/routes/routes.go
@@ -12,10 +12,12 @@ func Routes(r *chi.Mux, cfg *controllers.Config) {
 	r.Get("/classes", cfg.GetClasses)
 
 	// r.Get("/flashcard_sets", cfg.GetUsersFlashCardSets)
-	r.Get("/flashcard_set", cfg.GetFlashCardSet)
-	r.Post("/flashcard_set", cfg.CreateFlashCardSet)
-	r.Put("/flashcard_set", cfg.UpdateFlashCardSet)
-	r.Delete("/flashcard_set", cfg.DeleteFlashCardSet)
+	r.Route("/flashcard_set", func(r chi.Router) {
+		r.Post("/", cfg.CreateFlashCardSet)
+		r.Get("/", cfg.GetFlashCardSet)
+		r.Put("/", cfg.UpdateFlashCardSet)
+		r.Delete("/", cfg.DeleteFlashCardSet)
+	})
 
 	r.Get("/flashcard", cfg.GetFlashCard)
 	r.Post("/flashcard", cfg.CreateFlashCard)
