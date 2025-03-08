ecc0b46fd2a8894978f0bd3c3e8fd943c22d7ade
Brennan Cieslar <bac2102@hsutx.edu>
2025-03-08 13:57:51 -0600

    Fixed formatting on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
-import { IonReactRouter } from "@ionic/react-router";
-import { Route } from "react-router-dom";
-import Index from "./pages/Index";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import ClassDetail from "./pages/ClassDetail";
-import TeacherDashboard from "./pages/TeacherDashboard";
-import PublicCards from "./pages/PublicCards";
-import UserAccount from "./pages/UserAccount";
+import { Toaster } from '@/components/ui/toaster';
+import { Toaster as Sonner } from '@/components/ui/sonner';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { Route } from 'react-router-dom';
+import Index from './pages/Index';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import ClassDetail from './pages/ClassDetail';
+import TeacherDashboard from './pages/TeacherDashboard';
+import PublicCards from './pages/PublicCards';
+import UserAccount from './pages/UserAccount';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -51,18 +51,18 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <IonApp>
-          <IonReactRouter>
-            <IonRouterOutlet>
-              <Route exact path="/" component={Index} />
-              <Route exact path="/home" component={Home} />
-              <Route exact path="/class/:id" component={ClassDetail} />
-              <Route exact path="/teacher" component={TeacherDashboard} />
-              <Route exact path="/teacher/class/:id" component={ClassDetail} />
-              <Route exact path="/public-cards" component={PublicCards} />
-              <Route exact path="/userAccount" component={UserAccount} />
-              <Route component={NotFound} />
-            </IonRouterOutlet>
-          </IonReactRouter>
+        <IonReactRouter>
+          <IonRouterOutlet>
+            <Route exact path="/" component={Index} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/class/:id" component={ClassDetail} />
+            <Route exact path="/teacher" component={TeacherDashboard} />
+            <Route exact path="/teacher/class/:id" component={ClassDetail} />
+            <Route exact path="/public-cards" component={PublicCards} />
+            <Route exact path="/userAccount" component={UserAccount} />
+            <Route component={NotFound} />
+          </IonRouterOutlet>
+        </IonReactRouter>
       </IonApp>
       <Toaster />
       <Sonner />
