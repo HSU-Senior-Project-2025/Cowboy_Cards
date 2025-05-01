217ba0993bbc6be219c7edc26fb87291cc7a69c8
SLajuwomi <stephenlajuwomi@gmail.com>
2025-05-01 13:24:24 -0500

    removed cards mastered/studied from UserAccount

--- src/components/UserAccountFirstRow.tsx
+++ src/components/UserAccountFirstRow.tsx
@@ -90,10 +90,6 @@ const UserAccountFirstRow = (props) => {
               value={props.userInfo?.login_streak}
             />
             <InfoRow label="Classes Taken" value={props.userInfo?.numClasses} />
-            {/* <InfoRow
-              label="Cards Mastered/Studied"
-              value={`${props.userInfo?.cardsMastered} / ${props.userInfo?.cardsStudied}`}
-            /> */}
             <InfoRow
               label="Total Card Views"
               value={props.userInfo?.totalCardViews}
