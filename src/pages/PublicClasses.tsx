6695c4f3bedba6d8994928aac376ddd7cf643f28
SLajuwomi <stephenlajuwomi@gmail.com>
2025-05-16 15:14:05 -0500

    reset to undo rebase again fixed no succed toast on public cards/classes

--- src/pages/PublicClasses.tsx
+++ src/pages/PublicClasses.tsx
@@ -12,6 +12,7 @@ import {
   IonContent,
   IonPage,
   IonSearchbar,
+  useIonToast,
 } from '@ionic/react';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
@@ -21,6 +22,7 @@ const PublicClasses = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const [searchText, setSearchText] = useState('');
+  const [presentToast] = useIonToast();
   const { data: userClasses, isLoading: isLoadingUserClasses } =
     useUserClasses();
 
@@ -57,37 +59,37 @@ const PublicClasses = () => {
     <IonPage>
       <Navbar />
       <IonContent>
-        <div id="main-content" className="container mx-auto px-0 py-8 w-4/5">
-          <div className="flex items-center flex-col justify-between mb-4">
-            <h1 className="text-4xl tracking-wide font-bold font-smokum pb-8">
+        <div id='main-content' className='container mx-auto px-0 py-8 w-4/5'>
+          <div className='flex items-center flex-col justify-between mb-4'>
+            <h1 className='text-4xl tracking-wide font-bold font-smokum pb-8'>
               Public Classes
             </h1>
             {(loading || isLoadingUserClasses) && <div>Loading...</div>}
-            {error && <div className="text-red-500 mt-2">{error}</div>}
+            {error && <div className='text-red-500 mt-2'>{error}</div>}
             <IonSearchbar
               value={searchText} // eslint-disable-next-line
               onIonInput={(e: any) => setSearchText(e.target.value)}
-              placeholder="Search classes"
-              className="mb-4 max-w-lg"
+              placeholder='Search classes'
+              className='mb-4 max-w-lg'
             />
           </div>
-          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+          <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
             {filteredClasses?.map(
               (classItem) =>
                 !isUserMember(classItem.ID) && (
                   <Link key={classItem.ID} to={`/class/${classItem.ID}`}>
-                    <IonCard className="cursor-pointer hover:shadow-lg hover:-translate-y-1 transition-transform-shadow duration-200 rounded-lg border shadow-sm">
-                      <IonCardHeader className="flex flex-col space-y-1.5 p-6">
-                        <IonCardTitle className="text-2xl font-semibold leading-none tracking-tight">
+                    <IonCard className='cursor-pointer hover:shadow-lg hover:-translate-y-1 transition-transform-shadow duration-200 rounded-lg border shadow-sm'>
+                      <IonCardHeader className='flex flex-col space-y-1.5 p-6'>
+                        <IonCardTitle className='text-2xl font-semibold leading-none tracking-tight'>
                           {classItem.ClassName}
                         </IonCardTitle>
-                        <IonCardSubtitle className="text-sm ">
+                        <IonCardSubtitle className='text-sm '>
                           {classItem.ClassDescription || 'No description'}
                         </IonCardSubtitle>
                         <IonButton
-                          expand="block"
-                          color="primary"
-                          className="mt-4"
+                          expand='block'
+                          color='primary'
+                          className='mt-4'
                           onClick={async () => {
                             try {
                               const response = await makeHttpCall(
@@ -101,6 +103,11 @@ const PublicClasses = () => {
                                 }
                               );
                               console.log('Join class response:', response);
+                              presentToast({
+                                message: 'Successfully joined class',
+                                duration: 2000,
+                                color: 'success',
+                              });
                             } catch (error) {
                               console.error('Error joining class:', error);
                             }
